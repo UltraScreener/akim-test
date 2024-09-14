58e8c368b5166b64663363456ff8dab017903cc8
UltraScreener <web.c1nn1k@gmail.com>
2024-09-14 20:23:48 +0300

    request

--- src/components/ui/playButton.jsx
+++ src/components/ui/playButton.jsx
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const PlayButton = () => {
     const [play, setPlay] = useState(false)
+    
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(async response => {
+        setData(await response.json())
+    })
+    }, [])
+
 
     const onClick = (e) => {
         e.stopPropagation()
@@ -9,10 +19,14 @@ const PlayButton = () => {
         console.log(play)
         setPlay(!play)
     }
+    
     return (
+        <>
+        <div style={{color: '#fff'}}>responseTitle: {data.title}</div>
         <button type="button" onClick={onClick}>
             Image
         </button>
+        </>
     )
 }
 
