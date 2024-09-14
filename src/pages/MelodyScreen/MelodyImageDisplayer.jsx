57d06e502b48376d5c4e137c1f9e95d0e7d58cbc
UltraScreener <web.c1nn1k@gmail.com>
2024-09-14 19:53:17 +0300

    Пример изменения на public image

--- src/pages/MelodyScreen/MelodyImageDisplayer.jsx
+++ src/pages/MelodyScreen/MelodyImageDisplayer.jsx
@@ -4,8 +4,8 @@ import image from "../../assets/images/melody-image.png"
 const MelodyImageDisplayer = ({imagePath}) => {
     return (
         <div className="melody-image-displayer">
-            <img className="melody-disk-blur" width="400" height="400" src={image} alt="" />
-            <img className="melody-disk" width="300" height="300" src={image} alt="" />
+            <img className="melody-disk-blur" width="400" height="400" src={'/images/melody-image.png'} alt="" />
+            <img className="melody-disk" width="300" height="300" src={'/images/melody-image.png'} alt="" />
         </div>
     )
 }
